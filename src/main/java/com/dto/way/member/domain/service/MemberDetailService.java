a299c114f86c68c669a3da43e49f7faf0e865430
Haecheol Shin <dev.hc.shin@gmail.com>
2024-04-11 10:11:24 +0900

    DTO-120 Fix: JWT 기능을 구현한다.
    - 토큰을 발급할 때 authorities가 null값이 넘어오는 문제가 발생했습니다.

---
+++ src/main/java/com/dto/way/member/domain/service/MemberDetailService.java
@@ -0,0 +1,33 @@
+package com.dto.way.member.domain.service;
+
+import com.dto.way.member.domain.entity.Member;
+import com.dto.way.member.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userDetailsService")
+@RequiredArgsConstructor
+public class MemberDetailService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(email)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("해당하는 회원을 찾을 수 없습니다."));
+    }
+
+    private UserDetails createUserDetails(Member member) {
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .build();
+    }
+
+
+}
