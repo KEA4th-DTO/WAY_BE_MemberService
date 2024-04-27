27226deb5737eca40ed168081e80fe52817b902c
Haecheol Shin <dev.hc.shin@gmail.com>
2024-04-28 03:20:11 +0900

    DTO-261 Feat: 로그인 검증 로직 구현 완료
    - 로그인 검증 구현이 완료되었습니다.

--- src/main/java/com/dto/way/member/web/dto/MemberRequestDto.java
+++ src/main/java/com/dto/way/member/web/dto/MemberRequestDto.java
@@ -44,6 +44,8 @@ public class MemberRequestDto {
         private String email;
 
         @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+        @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
+                message = "영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호를 입력해주세요.")
         private String password;
     }
 }
--- src/main/java/com/dto/way/member/web/response/code/status/ErrorStatus.java
+++ src/main/java/com/dto/way/member/web/response/code/status/ErrorStatus.java
@@ -16,13 +16,15 @@ public enum ErrorStatus implements BaseErrorCode {
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"COMMON401","인증이 필요합니다."),
     _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
-    // 이 곳에 응답을 추가합니다.
+    // 회원가입 응답
     MEMBER_NICKNAME_NOT_VALID(HttpStatus.BAD_REQUEST, "MEMBER4001", "유효한 닉네임이 아닙니다."),
     MEMBER_NICKNAME_DUPLICATED(HttpStatus.BAD_REQUEST, "MEMBER4002", "중복된 닉네임 입니다."),
     MEMBER_PASSWORD_NOT_VALID(HttpStatus.BAD_REQUEST, "MEMBER4003", "유효한 비밀번호가 아닙니다."),
     MEMBER_PASSWORD_NOT_MATCHED(HttpStatus.BAD_REQUEST, "MEMBER4004", "비밀번호가 일치하지 않습니다."),
     MEMBER_EMAIL_DUPLICATED(HttpStatus.BAD_REQUEST, "MEMBER4005", "해당 이메일로 가입한 계정이 존재합니다."),
 
+    // 로그인 응답
+    MEMBER_LOGIN_FAILED(HttpStatus.BAD_REQUEST, "MEMBER40010", "아이디 또는 비밀번호가 일치하지 않습니다."),
     ;
 
 
