46739e3d0c571e8c890216343667c840150989ee
jmin314 <jmin314@naver.com>
2024-05-18 01:00:38 +0900

    new.. push and ignore

--- src/test/java/com/dto/way/member/ApplicationTests.java
+++ src/test/java/com/dto/way/member/ApplicationTests.java
@@ -3,11 +3,10 @@ package com.dto.way.member;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class ApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() {}
 
 }
