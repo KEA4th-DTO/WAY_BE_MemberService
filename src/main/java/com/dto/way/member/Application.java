4cb730b493c7d0ab3e2dd18197ae14f96e86c9fc
jmin314 <jmin314@naver.com>
2024-05-18 00:12:25 +0900

    property set.

--- src/main/java/com/dto/way/member/Application.java
+++ src/main/java/com/dto/way/member/Application.java
@@ -3,11 +3,12 @@ package com.dto.way.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.setProperty("spring.config.name","application");
 	}
 
 }
