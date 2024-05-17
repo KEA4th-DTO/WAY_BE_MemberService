4377ed061e6a75407519eec6602afe6ac8ff8169
jmin314 <jmin314@naver.com>
2024-05-18 00:22:41 +0900

    roll back

--- src/main/java/com/dto/way/member/Application.java
+++ src/main/java/com/dto/way/member/Application.java
@@ -2,14 +2,9 @@ package com.dto.way.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-		System.setProperty("spring.config.name","application");
-	}
+	public static void main(String[] args) {SpringApplication.run(Application.class, args);}
 
 }
