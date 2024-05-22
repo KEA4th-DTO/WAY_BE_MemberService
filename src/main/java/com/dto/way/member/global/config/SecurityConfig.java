3724207914f8b30607d11b29f99683a6efe051d6
Haecheol Shin <dev.hc.shin@gmail.com>
2024-05-22 16:34:14 +0900

    Feat: Swagger UI 추가

--- src/main/java/com/dto/way/member/global/config/SecurityConfig.java
+++ src/main/java/com/dto/way/member/global/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .sessionManagement(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**").permitAll()
                         .requestMatchers("/member-service/login").permitAll()
                         .requestMatchers("/member-service/signup").permitAll()
                         .requestMatchers("/member-service/logout").permitAll()
